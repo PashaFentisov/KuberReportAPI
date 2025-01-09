7426f913188141219481449edcc71beb2c269ed0
Pasha Fentisov <pasha16.ua@gmail.com>
2025-01-09 23:47:46 +0200

    implement report generating service, add docker-compose.yaml for local kafka setup

--- src/main/java/core/kuberreportapi/KafkaReportGeneratorService.java
+++ src/main/java/core/kuberreportapi/KafkaReportGeneratorService.java
@@ -1,17 +1,29 @@
 package core.kuberreportapi;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class KafkaReportGeneratorService implements ReportGeneratorService {
-    private final KafkaTemplate<String, UserInfoDto> kafkaTemplate;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
 
     @Override
     public String generateReport(UserInfoDto userInfoDto) {
-        kafkaTemplate.send("report", userInfoDto);
+        String userInfoJson;
+        try {
+            userInfoJson = objectMapper.writeValueAsString(userInfoDto);
+        } catch (JsonProcessingException e) {
+            log.error("Error while converting UserInfoDto to JSON", e);
+            return "Error while generating report";
+        }
+        kafkaTemplate.send("report", userInfoJson);
         return "Report is generating and will be sent to your email";
     }
 }
--- src/main/java/core/kuberreportapi/UserInfoDto.java
+++ src/main/java/core/kuberreportapi/UserInfoDto.java
@@ -2,8 +2,13 @@ package core.kuberreportapi;
 
 import lombok.Data;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Data
-public class UserInfoDto {
+public class UserInfoDto implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
     private String name;
     private String email;
 }
